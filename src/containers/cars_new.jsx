f742d0d370fe79574bf7bfe1d5f3152c7b4c9fbc
Rachael Savoy <rachael.a.hakim@gmail.com>
2021-01-27 17:38:35 +0900

    FEAT: new car container.

--- src/containers/cars_new.jsx
+++ src/containers/cars_new.jsx
@@ -23,7 +23,35 @@ class CarsNew extends Component {
         <form onSubmit={this.props.handleSubmit(this.onSumbit)}>
           <div className="form-group">
             <label htmlFor="InputBrand">Brand</label>
-            <Field name="brand" type="text"
-    ]
+            <Field name="brand" type="text" placeholder="Aston Martin" component="input" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="InputModel">Model</label>
+            <Field name="owner" type="text" placeholder="DB Mark III" component="input" classNmae="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="InputOwner">Owner</label>
+            <Field name="owner" type="text" placeholder="James Bond" component="input" className="form-control" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="InputPlate">Plate</label>
+            <Field name="model" type="text" placeholder="DB Mark III" component="input" className="form-control" />
+          </div>
+          <button type="submit"> Add car</button>
+        </form>
+      </div>
+    ];
   }
+};
+
+function mapStateToProps(state) {
+  return {
+    garage: state.garage
+  };
 }
+
+export default reduxForm({
+  form: 'newCarForm'
+})(
+  connect(mapStateToProps, {addCar })(CarsNew)
+);
