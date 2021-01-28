66f1132e3139cb6f67d80100b8aa7effc4ebb71b
Rachael Savoy <rachael.a.hakim@gmail.com>
2021-01-28 17:14:39 +0900

    FIX: reindented, fix spelling errors.

--- src/containers/cars_new.jsx
+++ src/containers/cars_new.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { connect } from 'raect-redux';
+import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 
@@ -8,7 +8,7 @@ import { addCar } from '../actions';
 
 class CarsNew extends Component {
   onSubmit = (values) => {
-    this.pros.addCar(this.props.garage, values, () => {
+    this.props.addCar(this.props.garage, values, () => {
       this.props.history.push('/');
     });
   }
@@ -16,28 +16,28 @@ class CarsNew extends Component {
   render () {
     return [
       <Aside key="aside" garage={this.props.garage}>
-        <Link to="/"> Back to list </List>
+        <Link to="/">Back to list</Link>
       </Aside>,
       <div key="add" className="form-container" style={{ backgroundImage: "url('/assets/images/form.jpg')"}}>
         <div className="overlay"></div>
-        <form onSubmit={this.props.handleSubmit(this.onSumbit)}>
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
           <div className="form-group">
             <label htmlFor="InputBrand">Brand</label>
             <Field name="brand" type="text" placeholder="Aston Martin" component="input" className="form-control" />
           </div>
           <div className="form-group">
             <label htmlFor="InputModel">Model</label>
-            <Field name="owner" type="text" placeholder="DB Mark III" component="input" classNmae="form-control" />
+            <Field name="model" type="text" placeholder="DB Mark III" component="input" className="form-control" />
           </div>
           <div className="form-group">
             <label htmlFor="InputOwner">Owner</label>
             <Field name="owner" type="text" placeholder="James Bond" component="input" className="form-control" />
           </div>
-          <div className="form-control">
+          <div className="form-group">
             <label htmlFor="InputPlate">Plate</label>
-            <Field name="model" type="text" placeholder="DB Mark III" component="input" className="form-control" />
+            <Field name="plate" type="text" placeholder="DB Mark III" component="input" className="form-control" />
           </div>
-          <button type="submit"> Add car</button>
+          <button type="submit">Add car</button>
         </form>
       </div>
     ];
@@ -51,7 +51,8 @@ function mapStateToProps(state) {
 }
 
 export default reduxForm({
-  form: 'newCarForm'
+  form: 'newCarForm' // a unique identifier
 })(
-  connect(mapStateToProps, {addCar })(CarsNew)
+  connect(mapStateToProps, { addCar })(CarsNew)
 );
+
--- src/containers/cars_show.jsx
+++ src/containers/cars_show.jsx
@@ -1,4 +1,4 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
@@ -6,9 +6,9 @@ import { Link, withRouter } from 'react-router-dom';
 import Aside from '../components/aside';
 import { removeCar } from '../actions';
 
-class CarsShow extends Component{
+class CarsShow extends Component {
   handleClick = () => {
-    this.props.remoteCar(this.props.history, this.props.car);
+    this.props.removeCar(this.props.history, this.props.car);
   }
 
   render () {
@@ -17,25 +17,25 @@ class CarsShow extends Component{
       return (
         <Aside key="aside" garage={this.props.garage}>
           <Link to="/">Back to list</Link>
-        </Aisde>);
+        </Aside>);
     }
     return [
       <Aside key="aside" garage={this.props.garage}>
         <Link to="/">Back to list</Link>
-      </Aside>
+      </Aside>,
       <div className="car-container" key="car">
         <div className="car-card">
-          <img className="car-picture" src="/assets/images/logo_square.svg"/>
+          <img className="car-picture" src="/assets/images/logo_square.svg" />
           <div className="car-details">
             <span>{car.brand} - {car.model}</span>
             <ul>
               <li><strong>Owner:</strong> {car.owner}</li>
             </ul>
-            <span className="plate">(car.plate)</span>
+            <span className="plate">{car.plate}</span>
           </div>
           <button className="delete" onClick={this.handleClick}>
             <i className="fa fa-trash-o" aria-hidden="true"></i>
-            delete
+            Delete
           </button>
         </div>
       </div>
@@ -55,4 +55,3 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(CarsShow));
-""
